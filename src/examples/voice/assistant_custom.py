20d00c7c5219437287accde034e89cb569542f75
LAPTOP-IFRJGHMT\hongy <hong.yew@gmail.com>
2018-10-23 15:00:12 +1100

    custom script

---
+++ src/examples/voice/assistant_custom.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+"""A demo of the Google Assistant GRPC recognizer."""
+
+import logging
+
+import aiy.assistant.grpc
+import aiy.audio
+import aiy.voicehat
+import subprocess
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s] %(levelname)s:%(name)s:%(message)s"
+)
+
+
+def main():
+    status_ui = aiy.voicehat.get_status_ui()
+    status_ui.status('starting')
+    assistant = aiy.assistant.grpc.get_assistant()
+    button = aiy.voicehat.get_button()
+    status_ui.status('ready')
+    print('Press the button and speak')
+    button.wait_for_press()
+    status_ui.status('listening')
+
+    with aiy.audio.get_recorder():
+        while True:
+
+            print('Listening...')
+            text, audio = assistant.recognize()
+            if text:
+                if text == 'shut down':
+                    print("Shutting down, goodbye")
+                    subprocess.call("sudo shutdown now", shell=True)
+                    break
+                if text == 'reboot':
+                    print("Rebooting")
+                    subprocess.call("sudo shutdown -r now", shell=True)
+                    break
+                if text == 'goodbye':
+                    status_ui.status('stopping')
+                    print('Bye!')
+                    break
+                print('You said "', text, '"')
+
+            if audio:
+                aiy.audio.play_audio(audio, assistant.get_volume())
+
+
+if __name__ == '__main__':
+    main()
