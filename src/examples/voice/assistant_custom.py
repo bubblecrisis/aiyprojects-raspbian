f0c9c996787ad1857e13a4a5cd06a33e5935a450
Hong Yew <bubblecrisis@gmail.com>
2018-10-24 16:13:56 +1100

    cleaner calculation

--- src/examples/voice/assistant_custom.py
+++ src/examples/voice/assistant_custom.py
@@ -18,10 +18,8 @@ logging.basicConfig(
 
 def launch_phrase():
     read = wave.open('./launch/nab.wav','rb')
-    framesize = read.getsampwidth() * read.getnchannels()
-    total_size = read.getnframes() * framesize
-    number_of_chunks = int(read.getnframes() / 3200)
-    chunk_size = int(read.getnframes() / number_of_chunks)
+    chunk_size = 3200
+    number_of_chunks = int(read.getnframes() / chunk_size)
     
     data_frames = []
     read.rewind()             
